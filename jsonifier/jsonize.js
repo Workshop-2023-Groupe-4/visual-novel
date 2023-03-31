ed0b8a7e1ff6a3eb04eed87b3d6a751565cc354a
Arthaud <arthaudp33@gmail.com>
2023-03-31 18:37:38 +0200

    feat: display twee story

--- jsonifier/jsonize.js
+++ jsonifier/jsonize.js
@@ -3,12 +3,12 @@ const lineReader = require('line-reader');
 const {getJsonFromLines} = require("./tweeToJson");
 
 const input = './stories/amnesia.twee';
-const output = './data/stories/amnesia.twee';
+const output = './data/stories/amnesia.json';
 
 const lines = [];
-lineReader.eachLine(input, function(line, last) {
+lineReader.eachLine(input, function (line, last) {
     lines.push(line)
-    if(last) {
+    if (last) {
         fs.writeFileSync(output, getJsonFromLines(lines))
     }
 });
