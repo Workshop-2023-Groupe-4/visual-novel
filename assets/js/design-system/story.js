f6f57c903d4b40cd09711c6691ed7903032a9023
Arthaud <arthaudp33@gmail.com>
2023-04-01 22:21:47 +0200

    feat: show passage when clicking on next

---
+++ assets/js/design-system/story.js
@@ -0,0 +1,54 @@
+const CLASSES = {
+    hidden: 'hidden',
+    storyPassage: 'story__passage',
+};
+
+export default class Story {
+    constructor() {
+        this.passagesElement = document.getElementById('storyPassages');
+
+        this.startPassage = this.passagesElement.dataset.start;
+
+        this.#listen();
+    }
+
+    // méthode privée, voir https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/Private_class_fields
+    #listen() {
+        window.addEventListener('popstate', event => {
+            this.showPassage(document.location.hash)
+        });
+    }
+
+    getPassageElementFromAnchor(anchor) {
+        let id = anchor.replace('#', '');
+        return document.getElementById(id);
+    }
+
+    hideAllPassages() {
+        let allPassagesElements = this.passagesElement.querySelectorAll('.' + CLASSES.storyPassage);
+
+        allPassagesElements.forEach(el => {
+            el.classList.add(CLASSES.hidden);
+        })
+    }
+
+    showPassage(passageAnchor) {
+        let passage = this.getPassageElementFromAnchor(passageAnchor);
+
+        if (!passage) {
+            return;
+        }
+
+        this.hideAllPassages();
+        passage.classList.remove(CLASSES.hidden);
+    }
+
+    start() {
+        if (document.location.hash) {
+            this.showPassage(document.location.hash);
+            return;
+        }
+
+        this.showPassage(this.startPassage);
+    }
+}
---
+++ assets/js/in-story.js
@@ -0,0 +1,5 @@
+import Story from './design-system/story';
+
+window.story = new Story();
+
+story.start();
