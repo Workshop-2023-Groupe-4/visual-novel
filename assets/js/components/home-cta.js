982e05db8bd454fe82eaf1fa4d715800e4f055b0
Olivia206 <olivia.simonet@mmibordeaux.com>
2023-04-13 12:40:13 +0200

    fixed js btn

--- assets/js/components/home-cta.js
+++ assets/js/components/home-cta.js
@@ -1,34 +1,36 @@
 import { gsap } from "gsap";
 
 const homeCta = document.querySelector(".home-btn");
-const ctaTrigger = document.getElementById("home-sections");
 
-const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            gsap.to(homeCta, {
-                autoAlpha: 1,
-                display: "flex",
-                duration: .2,
-                ease: "power2",
-                y: 0,
-                x: -100
-            })
+if (homeCta) {
 
-        } else {
-            gsap.to(homeCta, {
-                autoAlpha: 0,
-                display: "none",
-                duration: .2,
-                ease: "power2",
-                y: 50,
-                x: -100
-            })
-        }
-    })
-}, {threshold: 0.2}
-)
-
-observer.observe(ctaTrigger);
-
-
+    const ctaTrigger = document.getElementById("home-sections");
+    
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                gsap.to(homeCta, {
+                    autoAlpha: 1,
+                    display: "flex",
+                    duration: .2,
+                    ease: "power2",
+                    y: 0,
+                    x: "-50%"
+                })
+    
+            } else {
+                gsap.to(homeCta, {
+                    autoAlpha: 0,
+                    display: "none",
+                    duration: .2,
+                    ease: "power2",
+                    y: 50,
+                    x: "-50%"
+                })
+            }
+        })
+    }, {threshold: 0.2}
+    )
+    
+    observer.observe(ctaTrigger);    
+}
