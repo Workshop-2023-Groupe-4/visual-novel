cff64054460f9df73730bf517fcdaf2cf61d2007
Olivia206 <olivia.simonet@mmibordeaux.com>
2023-04-14 12:49:32 +0200

    added animations

--- assets/js/components/StoryImage.js
+++ assets/js/components/StoryImage.js
@@ -22,6 +22,11 @@ export default class StoryImage {
             else if (image.className === "" || image.className === "center") {
                 this.scaleImage(image, 1.2);
             }
+            else if (image.className === "parallaxScale") {
+                const depth = image.dataset.depth;
+                const scale = image.dataset.scale;
+                this.parallaxScale(image, depth, scale);
+            }
             else if (image.className === "parallax") {
                 const depth = image.dataset.depth;
                 this.parallaxImage(image, depth);
@@ -57,11 +62,24 @@ export default class StoryImage {
             },
         }) 
     }
+    parallaxScale(image, ratio, scale) {
+        const movement = -(400 * ratio);
+        gsap.to(image, {
+            y: movement,
+            scale: scale,
+            ease: "none",
+            scrollTrigger: {
+                trigger: image,
+                scrub: true,
+                start: "-=200",
+                end: "bottom top"
+            },
+        }) 
+    }
     parallaxImage(image, ratio) {
         const movement = -(400 * ratio);
         gsap.to(image, {
             y: movement,
-            scale: 2,
             ease: "none",
             scrollTrigger: {
                 trigger: image,
